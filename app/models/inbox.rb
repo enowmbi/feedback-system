5de593babb5ba7ee23f78583f2c1a6f53245baaf
Enow Mbi <benowmbi@yahoo.com>
2022-01-10 20:51:18 +0100

    Add presence, length and uniqueness validation on name column of inbox table

--- app/models/inbox.rb
+++ app/models/inbox.rb
@@ -1,2 +1,8 @@
 class Inbox < ApplicationRecord
+  MIN_LENGTH = 6
+  MAX_LENGTH = 30
+
+  validates :name, presence: true
+  validates :name, length: { in: MIN_LENGTH..MAX_LENGTH }
+  validates :name, uniqueness: { case_sensitive: false }
 end
